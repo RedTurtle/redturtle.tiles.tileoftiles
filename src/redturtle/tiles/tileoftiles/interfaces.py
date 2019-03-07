d04fe8f7263bff0e93d3a63a7f37b2966d00b200
Piero Nicolli <piero.nicolli@redturtle.it>
2019-03-07 11:05:10 +0100

    Removed styles and other minor fixes

--- src/redturtle/tiles/tileoftiles/interfaces.py
+++ src/redturtle/tiles/tileoftiles/interfaces.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
-from plone.supermodel import model
-from zope import schema
 from plone.namedfile.field import NamedBlobFile
-from redturtle.tiles.tileoftiles import _
+from plone.supermodel import model
 from redturtle.tiles.management.interfaces import IRedturtleTilesManagementLayer
+from redturtle.tiles.tileoftiles import _
+from zope import schema
+
 
 class IRedturtleTilesTileoftilesLayer(IRedturtleTilesManagementLayer):
     """Marker interface that defines a browser layer."""
@@ -15,29 +14,30 @@ class ITileOfTiles(model.Schema):
     """ """
 
     title = schema.TextLine(
-        title=_('label_tile_title', u'Title title'),
-        required=False
+        title=_("label_tile_title", u"Title title"), required=False
     )
 
     background_color = schema.TextLine(
-        title=_('Please set a hex code'),
+        title=_("Please set a hex code"),
         description=_("This property will be overriden by background image"),
-        required=False
+        required=False,
     )
 
     background_image = NamedBlobFile(
-        title=_(u'Please, upload an image'),
-        required=False
-        )
+        title=_(u"Please, upload an image"), required=False
+    )
 
-    min_heigth = schema.TextLine(
-        title=_('Write here class or list of classes that will be added to the tile view'),
+    min_height = schema.TextLine(
+        title=_(
+            "Write here class or list of classes that will be added to the tile view"
+        ),
         default=u"200px",
         required=False,
     )
 
     css_class = schema.TextLine(
-        title=_('Write here class or list of classes that will be added to the tile view'),
+        title=_(
+            "Write here class or list of classes that will be added to the tile view"
+        ),
         required=False,
     )
-
--- src/redturtle/tiles/tileoftiles/tile.py
+++ src/redturtle/tiles/tileoftiles/tile.py
@@ -1,6 +1,5 @@
-#-*- encoding:utf-8 *-
+# -*- encoding:utf-8 *-
 from plone import tiles
-from plone.app.uuid.utils import uuidToObject
 import base64
 
 
@@ -9,13 +8,17 @@ class TileOfTiles(tiles.PersistentTile):
     """
 
     def get_bg_style(self):
-        style = ''
-        bgcolor = self.data.get('background_color', None)
+        style = ""
+        bgcolor = self.data.get("background_color", None)
         if bgcolor:
-            style = 'background-color: {}'.format(bgcolor)
-        bgimage = self.data.get('background_image', None)
+            style = "background-color: {}".format(bgcolor)
+        bgimage = self.data.get("background_image", None)
         if bgimage:
             b64 = base64.b64encode(bgimage.data)
-            style = 'background-image: url("data:image/png;base64,{}")'.format(b64)
-        style = style + '; min-height: {}'.format( self.data.get('min_heigth', '20px'))
+            style = 'background-image: url("data:image/png;base64,{}")'.format(
+                b64
+            )
+        style = style + "; min-height: {}".format(
+            self.data.get("min_height", "20px")
+        )
         return style
